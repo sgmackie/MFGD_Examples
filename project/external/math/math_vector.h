a72ccdfceb6d623aac9105d4e6b1612cdc2de506
Sam Mackie <sam.g.mackie@googlemail.com>
2018-09-10 13:22:02 +0100

    math_vector.h: Added function to find vector length

--- project/external/math/math_vector.h
+++ project/external/math/math_vector.h
@@ -1,20 +1,20 @@
 #ifndef math_vector_H
 #define math_vector_H
 
-typedef struct VECTOR2D
-{
-    union //Either access individual X & Y values or as an array that can be indexed through
-    {
-        struct
-        {
-            float X, Y;
-        };
+#include <math.h>
 
-        float Elements[1];
+typedef union VECTOR2D //Either access individual X & Y values or as an array that can be indexed through
+{
+    struct
+    {
+        float X, Y;
     };
+
+    float Elements[1];
 
 } VECTOR2D;
 
+//Add two vectors
 inline VECTOR2D operator+(VECTOR2D A, VECTOR2D B)
 {
     VECTOR2D Result;
@@ -25,12 +25,23 @@ inline VECTOR2D operator+(VECTOR2D A, VECTOR2D B)
     return Result;
 }
 
+//Subtract two vectors
 inline VECTOR2D operator-(VECTOR2D A, VECTOR2D B)
 {
     VECTOR2D Result;
 
     Result.X = A.X - B.X;
     Result.Y = A.Y - B.Y;
+
+    return Result;
+}
+
+//Find the length of a vector
+float vector_FindLength(VECTOR2D A)
+{
+    float Result;
+
+    Result = sqrt(A.X * A.X + A.Y * A.Y);
 
     return Result;
 }
